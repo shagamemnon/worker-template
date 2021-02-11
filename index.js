89dfd4dddeb47a62b2e160791389c02e6fcf8a0b
Frank Taylor <franklin@cloudflare.com>
2021-02-11 14:02:55 -0500

    created template v1

--- index.js
+++ index.js
@@ -1,12 +1,43 @@
-addEventListener('fetch', event => {
-  event.respondWith(handleRequest(event.request))
-})
-/**
- * Respond with hello worker text
- * @param {Request} request
- */
+addEventListener('fetch', event => event.respondWith(handleRequest(event.request)))
+
+
 async function handleRequest(request) {
-  return new Response('Hello worker!', {
-    headers: { 'content-type': 'text/plain' },
-  })
+  let url = new URL(request.url)
+  let cookie = parseCookie(request.headers.get('cookie'))
+
+  if (cookie && cookie['__cfwlb']) {
+    url.hostname = atob(cookie['__cfwlb'])
+    response = await fetch(url.toString(), request)
+  } else {
+    url.hostname = origins.primary
+    response = await fetch(url.toString(), request)
+    response = new Response(response.body, response)
+    response.headers.set('Set-Cookie', `__cfwlb=${btoa(origins.primary)}; Domain=${request.headers.get('Host')}; Secure; HttpOnly`)
+  }
+
+  console.log(`Origin: ${url.hostname}`)
+  return response
+}
+
+
+const origins = {
+  primary: 'www.httpbin.org',
+  failover: 'docs.microsoft.com'
+}
+
+
+const parseCookie = (str) => {
+  try {
+    if (!str) throw 'no cookie'
+    return str
+      .split(';')
+      .map(v => v.split('='))
+      .reduce((acc, v) => {
+        acc[decodeURIComponent(v[0].trim())] = decodeURIComponent(v[1].trim())
+        return acc;
+      }, {})
+  } catch (e) {
+    console.log(e)
+    return null
+  }
 }
